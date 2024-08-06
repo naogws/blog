4718edc421b8b0539cf327b67af06a3b8ada1e63
Naufal Altaf <nao@altaf.xyz>
2024-08-06 19:37:14 +0700

    Bundle prettier and format the entire app
    - Prefer single quote.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "@/styles/globals.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import '@/styles/globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Naufal Altaf",
+  title: 'Naufal Altaf',
   description: "Naufal Altaf's personal blog.",
 };
 
@@ -15,10 +15,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        {children}
-      </body>
+    <html lang='en'>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
